3ffdd5e576edf49d38220e4fa424abf28caf61ec
RYUJEONGHUN <ryugh0210@naver.com>
2025-12-23 13:23:41 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -10,35 +10,34 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
+		int n=Integer.parseInt(br.readLine());
 		
-		int N=Integer.parseInt(br.readLine());
+		int[] arr=new int[n+1];
+		int[] dp=new int[n+1];
 		
-		int[] arr=new int[N];
-				
 		String[] temp=br.readLine().split(" ");
 		
-		int[] dp=new int[N];	
+		for(int i=0;i<n;i++) {
+			arr[i+1]=Integer.parseInt(temp[i]);
+		}
+
 		Arrays.fill(dp, 1);
 		
-		
-		for(int i=0;i<N;i++) {
-			arr[i]=Integer.parseInt(temp[i]);
-		}
-		
-		dp[0]=1;
-		
-		int realmax=1;
-		
-		for(int j=1;j<N;j++) {
-			for(int i=0;i<j;i++) {
-				if(arr[i]<arr[j]) {
-					dp[j]=Math.max(dp[j], dp[i]+1);
+		for(int i=2;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				if(arr[j]<arr[i]) {
+					dp[i]=Math.max(dp[j]+1, dp[i]);
 				}
 			}
-			realmax=Math.max(realmax, dp[j]);
 		}
+		int max=Integer.MIN_VALUE;
 		
-		System.out.print(realmax);
+		for(int i=1;i<=n;i++) {
+			if(dp[i]>max) {
+				max=dp[i];
+			}
+		}
+		System.out.print(max);
 	}
 
 }
