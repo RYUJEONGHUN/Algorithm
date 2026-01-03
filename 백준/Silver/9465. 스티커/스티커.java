b36d00111542452b5adbbdb7f5b3c39c0315bdca
RYUJEONGHUN <ryugh0210@naver.com>
2026-01-03 17:11:27 +0900

    [Silver I] Title: 스티커, Time: 592 ms, Memory: 135844 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -2,60 +2,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
-		String temp=br.readLine();
+		int T=Integer.parseInt(br.readLine());
 		
-		int T=Integer.parseInt(temp);
-
-		int[][] arr;
-		int[][] dp;
 		
 		for(int i=0;i<T;i++) {
 			int n=Integer.parseInt(br.readLine());
+			int[][] arr=new int[2][n];
+			int[][] dp=new int[3][n];
 			
-			arr=new int[2][n];
-			dp=new int[3][n];
 			
-			
-			String[] arg1=br.readLine().split(" ");
-			for(int j=0;j<n;j++) {
-				arr[0][j]=Integer.parseInt(arg1[j]);
+			for(int a=0;a<2;a++) {
+				String[] temp=br.readLine().split(" ");
+				for(int b=0;b<n;b++) {
+					arr[a][b]=Integer.parseInt(temp[b]);
+				}
 			}
 			
-			String[] arg2=br.readLine().split(" ");
-			for(int j=0;j<n;j++) {
-				arr[1][j]=Integer.parseInt(arg2[j]);
+			dp[0][0]=arr[0][0];
+			dp[1][0]=arr[1][0];
+			dp[2][0]=0;
+			
+			for(int k=1;k<n;k++) {
+				dp[0][k]=Math.max(dp[1][k-1], dp[2][k-1])+arr[0][k];
+				dp[1][k]=Math.max(dp[0][k-1], dp[2][k-1])+arr[1][k];
+				dp[2][k]=Math.max(Math.max(dp[0][k-1], dp[1][k-1]),dp[2][k-1]);
 			}
 			
-			
-			dp[0][0]=0;
-			dp[1][0]=arr[0][0];
-			dp[2][0]=arr[1][0];
-			
-			
-			
-			for(int j=1;j<n;j++) {			
-				dp[0][j]=Math.max(dp[0][j-1],Math.max(dp[1][j-1],dp[2][j-1]));
-				dp[1][j]=Math.max(dp[0][j-1]+arr[0][j],dp[2][j-1]+arr[0][j]);
-				dp[2][j]=Math.max(dp[0][j-1]+arr[1][j],dp[1][j-1]+arr[1][j]);
-				
-				
-			}
-			
-			int ans = Math.max(dp[0][n-1], Math.max(dp[1][n-1], dp[2][n-1]));
-			System.out.println(ans);
+			System.out.println(Math.max(Math.max(dp[0][n-1], dp[1][n-1]),dp[2][n-1]));
 		}
+		
+		
+		
 	}
-	
-
 
 }
